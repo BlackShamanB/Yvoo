348849599da877c2d8b5a1ca29155c2e0cbca462
--global <blackshaman2016@gmail.com>
2025-05-16 08:27:25 +0400

    home page

--- components/Home/hero.js
+++ components/Home/hero.js
@@ -7,7 +7,7 @@ import Link from "next/link";
 import EmbedVideo from "../EmbedVideo";
 import TypingEffect from "../TypingEffect";
 import ModalRequest from "@/components/ModalRequest";
-import LoadOnLoad from '@/components/LoadOnLoad';
+import LoadOnLoad from "@/components/LoadOnLoad";
 function Hero() {
   const [openModalRequest, setOpenModalRequest] = useState(false);
 
@@ -79,32 +79,41 @@ function Hero() {
         data-scroll
         data-scroll-speed="0.3"
       >
-      <LoadOnLoad>
-        <div className="3xl:max-w-[1516px] 2xl:max-w-[1116px] md:max-w-[598px] 3xl:w-[1516px] 2xl:w-[1116px] mx-[auto] md:pl-[0] pl-[20px] md:pr-[0] pr-[20px] pb-[126px]">
-          <h1
-            className="relative font-inter 2xl:text-[60px] md:text-[50px] text-[30px] font-[700] 2xl:leading-[68px] md:leading-[54px] leading-[34px] max-w-[830px] md:mb-[42px] mb-[20px]"
-            ref={elementRef}
-          >
-            AI-Powered On-Site Supplier Evaluation <span className="text-green">Scan</span>Pro+
-          </h1>
-          <div className="md:text-[24px] text-[18px] md:leading-[30px] leading-[26px] font-[700] md:mb-[42px] mb-[20px]">
-            Achieve audit excellence: scale resources, cut costs by 70%,<br/> and
-            accelerate timelines by 80%
-          </div>
-          <div className="text-black text-[16px] leading-[20px] font-[500] w-[max-content] p-[20px_64px] bg-blue rounded-[30px] md:mb-[60px] mb-[32px] uppercase"
-        onClick={handleModal}>
-            Book a demo
-          </div>
-          <div className="flex items-center mb-[8px]">
-            <div className="bg-green w-[12px] h-[12px] rounded-[50%] mr-[8px]"></div>
-            <div className="text-white text-[16px] leading-[20px] font-[500]">
-              Supplier audited by YVOO
+        <LoadOnLoad>
+          <div className="3xl:max-w-[1516px] 2xl:max-w-[1116px] md:max-w-[598px] 3xl:w-[1516px] 2xl:w-[1116px] mx-[auto] md:pl-[0] pl-[20px] md:pr-[0] pr-[20px] pb-[126px]">
+            <h1
+              className="relative font-inter 2xl:text-[60px] md:text-[50px] text-[30px] font-[700] 2xl:leading-[68px] md:leading-[54px] leading-[34px] max-w-[830px] md:mb-[42px] mb-[20px]"
+              ref={elementRef}
+            >
+              Ground Truth Supplier Superintelligence{" "}
+            </h1>
+            <div className="md:text-[24px] text-[18px] md:leading-[30px] leading-[26px] font-[700] md:mb-[60px] mb-[20px] max-w-[746px]">
+              Industry-first on-demand booking of on-site supplier evaluations -
+              delivered by qualified local experts, structured and enhanced
+              through AI-powered guidance and intelligent analysis.<br/>AI
+              standardizes evaluations, detects patterns, verifies supplier
+              data, and reveals hidden risks - turning reactive checks into
+              strategic intelligence.
+            </div>
+            <div
+              className="text-black text-[16px] leading-[20px] font-[500] w-[max-content] p-[20px_64px] bg-blue rounded-[30px] md:mb-[60px] mb-[32px] uppercase"
+              onClick={handleModal}
+            >
+              Exclusive Opportunity for New Clients!
+            </div>
+            <div className="font-[400] text-[20px] leading-[28px]">
+              Benefit from a 50% introductory discount on your first YVOO supplier evaluation
+            </div>
+            <div className="flex items-center mb-[8px]">
+              <div className="bg-green w-[12px] h-[12px] rounded-[50%] mr-[8px]"></div>
+              <div className="text-white text-[16px] leading-[20px] font-[500]">
+                Supplier audited by YVOO
+              </div>
+            </div>
+            <div className="text-green text-[24px] leading-[30px] font-[400]">
+              1.471
             </div>
           </div>
-          <div className="text-green text-[24px] leading-[30px] font-[400]">
-            1.471
-          </div>
-        </div>
         </LoadOnLoad>
       </div>
       <ModalRequest onClose={handleModal} visible={openModalRequest} />
